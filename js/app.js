226340c5cf7bcf86f3050422a63197707cc1cdc1
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-12 17:59:05 -0600

    Added the right result. I did it wrongly at the first time.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 let usarnames = [`Gabriel_winner`, `Alex_champion`, `Colleen_best`, `James_smart`, `Josh_clever`];
 
 let counter = 0;
-while(counter < usarnames.length) {
+while( counter < usarnames.length) {
+    let usarnames_to_lower_case = [];
+    usarnames_to_lower_case.push(usarnames[counter].toLowerCase());
     let usarnames_winner = usarnames[counter].includes(`winner`);
-    if(usarnames_winner === true) {
-        console.log(`We have a winner. -> ${usarnames[counter]}`);
+    if(usarnames_winner === true){
+        console.log(`We have a winner: ${usarnames_to_lower_case[counter]}`);
     }
     counter++;
 }
